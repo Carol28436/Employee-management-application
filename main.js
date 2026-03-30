60e38a2e5f07886efe687bd03dfd85ede04bf2ec
carol26436 <shirleyodondi@gmail.com>
2026-03-30 10:17:26 +0300

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+function showDetails() {
+    alert("Showing employee details...");
+}
+
+function editContact() {
+    alert("Edit feature coming soon!");
+}
+
+function deleteContact() {
+    let confirmDelete = confirm("Are you sure you want to delete?");
+    if (confirmDelete) {
+        alert("Deleted!");
+    }
+}
